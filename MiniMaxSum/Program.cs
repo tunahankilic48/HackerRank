4db27c94d6db7f3fa7e649b61c9fb78c14a4cc34
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-29 18:07:45 +0300

    Add project files.

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,17 @@
+﻿void miniMaxSum(List<int> arr)
+{
+    arr.Sort(); // Sort the array
+    long sum = 0, maxSum = 0, minSum = 0;
+
+    for (int i = 0; i < arr.Count; i++) // Find sum of the list
+    {
+        sum += arr[i];
+    }
+
+    maxSum = sum - arr[0]; // For max sum, first element shouldn't sum
+    minSum = sum - arr[arr.Count - 1]; // For min sum, last element shouldn't sum
+
+    Console.WriteLine(minSum + " " + maxSum);
+
+
+}
---
+++ PlusMinus/Program.cs
@@ -0,0 +1,28 @@
+﻿void plusMinus(List<int> arr)
+{
+    int positiveCounts = 0, negativeCounts = 0, zeroCounts = 0; // Variables were created for keeping count of the positive, negative and zero numbers.
+
+    foreach (int num in arr) // Loop were used to find out how many positive, negative and zero numbers in the list.
+    {
+        if (num > 0)
+        {
+            positiveCounts++;
+        }
+        else if (num < 0)
+        {
+            negativeCounts++;
+        }
+        else
+        {
+            zeroCounts++;
+        }
+    }
+
+    decimal positiveCountRatio = (decimal)positiveCounts / arr.Count; // Ratios (positiveNumbers/Length of the list) were calculated
+    decimal negativeCountRatio = (decimal)negativeCounts / arr.Count;
+    decimal zeroCountRatio = (decimal)zeroCounts / arr.Count;
+
+    Console.WriteLine(String.Format("{0:0.000000}", positiveCountRatio)); // Formatting was done for #.######
+    Console.WriteLine(String.Format("{0:0.000000}", negativeCountRatio));
+    Console.WriteLine(String.Format("{0:0.000000}", zeroCountRatio));
+}
