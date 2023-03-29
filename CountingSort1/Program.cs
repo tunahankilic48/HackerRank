7e8243fe80b1f68c49baf6e03739d62e0fcb12f7
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-29 21:44:14 +0300

    CountingSort

---
+++ CountingSort1/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> countingSort(List<int> arr)
+{
+
+    int[] result = new int[100];
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        result[arr[i]]++;
+    }
+
+    return result.ToList();
+}
