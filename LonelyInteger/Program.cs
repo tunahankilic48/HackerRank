95d4867f41f4e6a88b9bc2adbdcac5ebd459d250
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-29 18:57:33 +0300

    lonelyInteger

---
+++ LonelyInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿int lonelyinteger(List<int> a)
+{
+    a.Sort(); // List sorted.
+
+    List<int> temp = new List<int>(); // Temperary list created.
+
+    for (int i = 1; i < a.Count; i++)
+    {
+        if (a[i - 1] == a[i]) // In here, doublicated elements were found out and added to the temp list.
+        {
+            temp.Add(a[i - 1]);
+            temp.Add(a[i]);
+        }
+    }
+
+
+    return a.Except(temp).FirstOrDefault(); // The difference of the A list from the temporary list has been found which means uniqeu number in the list
+}
