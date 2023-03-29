c4723b7ccb7c1e55bad8247481abb46672e0de8e
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-29 18:38:55 +0300

    findMedian

---
+++ FindTheMedian/Program.cs
@@ -0,0 +1,10 @@
+﻿int findMedian(List<int> arr) // It is only for odd length
+{
+
+    arr.Sort();
+
+    decimal median = Math.Ceiling((decimal)arr.Count / 2);
+
+    return arr[(int)median - 1];
+
+}
