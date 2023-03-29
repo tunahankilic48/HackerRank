8073664940b82c0775865a6668409dd62eac347a
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-29 19:08:31 +0300

    DiagonalDifference

---
+++ DiagonalDifference/Program.cs
@@ -0,0 +1,14 @@
+﻿int diagonalDifference(List<List<int>> arr)
+{
+    int leftDiagonal = 0;
+    int rightDiagonal = 0;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        leftDiagonal += arr[i][i];
+        rightDiagonal += arr[i][arr.Count - 1 - i];
+    }
+
+
+    return Math.Abs(leftDiagonal - rightDiagonal);
+}
