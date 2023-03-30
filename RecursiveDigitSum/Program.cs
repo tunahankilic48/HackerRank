c8a756dc41c6562813d0704e33b10aaf037eaba7
Tunahan Kilic <tunahankilic942@gmail.com>
2023-03-30 04:22:43 +0300

    RecursiveDigitSum

---
+++ RecursiveDigitSum/Program.cs
@@ -0,0 +1,21 @@
+﻿int superDigit(string n, int k)
+{
+    n = n.Replace("0", "");
+    long sum = 0;
+
+    for (int i = 1; i < 10; i++)
+    {
+        string temp = n.Replace(i.ToString(), "");
+        sum += i * (n.Length - temp.Length);
+        n = temp;
+    }
+    sum *= k;
+    if (sum < 10)
+    {
+        return (int)sum;
+    }
+    else
+    {
+        return superDigit(sum.ToString(), 1);
+    }
+}
